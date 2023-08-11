6ef0979cf34537e1cc1c197fe6101d6979deaa6f
lhjok <lhjok@live.cn>
2023-08-12 04:18:56 +0800

    Update All Files

--- src/views/login.rs
+++ src/views/login.rs
@@ -3,107 +3,137 @@ use dioxus::prelude::*;
 use dioxus_router::prelude::*;
 use wasm_bindgen::prelude::*;
 use crate::route::Route;
+use web_sys::Element as WebSysElement;
+use gloo::events::EventListener;
+use gloo_utils::document;
 
 #[wasm_bindgen(module=
 "/static/scripts/elements.js")]
 extern {
-    #[wasm_bindgen(js_name= initDropdown)]
-    fn init_dropdown(id: &str);
+    type Input;
+    #[wasm_bindgen(constructor)]
+    fn new(el: &WebSysElement) -> Input;
+    #[wasm_bindgen(method)]
+    fn update(this: &Input);
+    
+    // #[wasm_bindgen(js_name= initInput)]
+    // fn init_input(id: &str);
+    // #[wasm_bindgen(js_name= initDropdown)]
+    // fn init_dropdown(id: &str);
+}
+
+fn init_input(id: &str) {
+    let trigger = document().query_selector_all(id).unwrap();
+    for i in 0..trigger.length() {
+        let element = trigger.item(i).unwrap().unchecked_into();
+        let input = Input::new(&element);
+        let _event = EventListener::new(&element, "click", move |event| {
+            event.prevent_default();
+			input.update();
+		});
+    }
 }
 
 pub fn Login(cx: Scope) -> Element {
     use_effect(cx, (), |()| async move {
-        init_dropdown("[data-te-dropdown-toggle-ref]");
+        init_input("[data-te-input-wrapper-init]");
+        // init_dropdown("[data-te-dropdown-toggle-ref]");
     });
-
-    // use_on_unmount(cx, move || {
-
-    // });
-
     render! {
-        div {
-            id: "login",
+        section {
+            class: "h-screen",
             div {
-                class: "m-8 relative",
-                "data-te-dropdown-ref": "",
-                button {
-                    r#type: "button",
-                    id: "dropdownMenuButton1",
-                    "data-te-dropdown-toggle-ref": "",
-                    "aria-expanded": "false",
-                    class: "flex btn-secondary",
-                    "Dropdown button",
-                    span {
-                        class: "ml-2 w-2",
-                        svg {
-                            class: "h-5 w-5",
-                            fill: "currentColor",
-                            "viewBox": "0 0 20 20",
-                            xmlns: "http://www.w3.org/2000/svg",
-                            path {
-                                d: "M5.23 7.21a.75.75 0 011.06.02L10 \
-                                    11.168l3.71-3.938a.75.75 0 111.08 \
-                                    1.04l-4.25 4.5a.75.75 0 01-1.08 \
-                                    0l-4.25-4.5a.75.75 0 01.02-1.06z",
-                                "fill-rule": "evenodd",
-                                "clip-rule": "evenodd"
-                            }
+                class: "container h-full px-6 py-24 mx-auto max-w-7xl",
+                div {
+                    class: "g-6 flex h-full flex-wrap items-center justify-center lg:justify-between",
+                    div {
+                        class: "mb-12 md:mb-0 md:w-8/12 lg:w-6/12",
+                        img {
+                            class: "w-full",
+                            src: "./static/images/draw2.webp",
+                            alt: "Phone image"
                         }
                     }
-                }
-                ul {
-                    "data-te-dropdown-menu-ref": "",
-                    "aria-labelledby": "dropdownMenuButton1",
-                    class: "ul-menu [&[data-te-dropdown-show]]:block",
-                    li {
-                        class: "li-menu",
-                        Link { to: Route::Home {}, "Go Home!" }
-                    }
-                    li {
-                        class: "li-menu",
-                        Link { to: Route::AdminIndex {}, "Go Admin!" }
-                    }
-                }
-            }
-            div {
-                class: "m-8 relative",
-                "data-te-dropdown-ref": "",
-                button {
-                    r#type: "button",
-                    id: "dropdownMenuButton1",
-                    "data-te-dropdown-toggle-ref": "",
-                    "aria-expanded": "false",
-                    class: "flex btn-secondary",
-                    "Dropdown button",
-                    span {
-                        class: "ml-2 w-2",
-                        svg {
-                            class: "h-5 w-5",
-                            fill: "currentColor",
-                            "viewBox": "0 0 20 20",
-                            xmlns: "http://www.w3.org/2000/svg",
-                            path {
-                                d: "M5.23 7.21a.75.75 0 011.06.02L10 \
-                                    11.168l3.71-3.938a.75.75 0 111.08 \
-                                    1.04l-4.25 4.5a.75.75 0 01-1.08 \
-                                    0l-4.25-4.5a.75.75 0 01.02-1.06z",
-                                "fill-rule": "evenodd",
-                                "clip-rule": "evenodd"
+                    div {
+                        class: "md:w-8/12 lg:ml-6 lg:w-5/12",
+                        form {
+                            div {
+                                class: "relative mb-6",
+                                "data-te-input-wrapper-init": "",
+                                input {
+                                    r#type: "text",
+                                    class: "peer m-input",
+                                    id: "exampleFormControlInput3",
+                                    placeholder: "Email address"
+                                }
+                                label {
+                                    r#for: "exampleFormControlInput3",
+                                    class: "m-label",
+                                    "帐号"
+                                }
+                            }
+                            div {
+                                class: "relative mb-6",
+                                "data-te-input-wrapper-init": "",
+                                input {
+                                    r#type: "password",
+                                    class: "peer m-input",
+                                    id: "exampleFormControlInput33",
+                                    placeholder: "Password"
+                                }
+                                label {
+                                    r#for: "exampleFormControlInput33",
+                                    class: "m-label",
+                                    "密码"
+                                }
+                            }
+                            div {
+                                class: "mb-6 flex items-center justify-between",
+                                div {
+                                    class: "mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]",
+                                    input {
+                                        class: "c-input",
+                                        r#type: "checkbox",
+                                        id: "exampleCheck3",
+                                        value: "",
+                                        checked: "",
+                                    }
+                                    label {
+                                        class: "inline-block pl-[0.15rem] hover:cursor-pointer",
+                                        r#for: "exampleCheck3",
+                                        "记住帐号"
+                                    }
+                                }
+                                a {
+                                    href: "#!",
+                                    class: "m-link",
+                                    "您忘记密码了吗?"
+                                }
+                            }
+                            button {
+                                r#type: "submit",
+                                class: "inline-block w-full items-center justify-center btn-secondary",
+                                "登录"
+                            }
+                            div {
+                                class: "my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t after:flex-1 \
+                                        before:border-neutral-300 after:mt-0.5 after:border-t after:border-neutral-300",
+                                p {
+                                    class: "mx-4 mb-0 text-center font-semibold dark:text-neutral-200",
+                                    "OR"
+                                }
+                            }
+                            button {
+                                class: "mb-5 flex w-full items-center justify-center btn-secondary",
+                                style: "background-color: #3b5998",
+                                Link { to: Route::Home {}, "注册用户" }
+                            }
+                            button {
+                                class: "flex w-full items-center justify-center btn-secondary",
+                                style: "background-color: #55acee",
+                                Link { to: Route::Home {}, "返回首页" }
                             }
                         }
-                    }
-                }
-                ul {
-                    "data-te-dropdown-menu-ref": "",
-                    "aria-labelledby": "dropdownMenuButton1",
-                    class: "ul-menu [&[data-te-dropdown-show]]:block",
-                    li {
-                        class: "li-menu",
-                        Link { to: Route::Home {}, "Go Home!" }
-                    }
-                    li {
-                        class: "li-menu",
-                        Link { to: Route::AdminIndex {}, "Go Admin!" }
                     }
                 }
             }
--- static/scripts/elements.js
+++ static/scripts/elements.js
@@ -1,14 +1,26 @@
-import { Dropdown } from '/node_modules/tw-elements/dist/js/tw-elements.es.min.js';
+import { Dropdown, Input } from '/node_modules/tw-elements/dist/js/tw-elements.es.min.js';
 
-export function initDropdown(opt) {
-	const trigger = document.querySelectorAll(opt);
-	const triggers = [].slice.call(trigger);
-	triggers.forEach((el) => {
-		const dropdown = new Dropdown(el);
-		const handler = (e) => {
-			e.preventDefault();
-			dropdown.toggle();
-		};
-		el.addEventListener('click', handler);
-	});
-}
+export { Dropdown, Input };
+// export function initDropdown(opt) {
+// 	const trigger = document.querySelectorAll(opt);
+// 	[...trigger].forEach((element) => {
+// 		const dropdown = new Dropdown(element);
+// 		const handler = (event) => {
+// 			event.preventDefault();
+// 			dropdown.toggle();
+// 		};
+// 		element.addEventListener('click', handler);
+// 	});
+// }
+
+// export function initInput(opt) {
+// 	const trigger = document.querySelectorAll(opt);
+// 	[...trigger].forEach((element) => {
+// 		const input = new Input(element);
+// 		const handler = (event) => {
+// 			event.preventDefault();
+// 			input.update();
+// 		};
+// 		element.addEventListener('click', handler);
+// 	});
+// }
